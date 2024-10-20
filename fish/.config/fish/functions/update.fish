553cf52ed1cc2dfe208a62b90bcafb1ee56b77ad
harry-wilkos <hg456wilkos@gmail.com>
2024-10-20 13:27:14 +0100

    addedd more clearer verbosity and git pulling

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -5,6 +5,21 @@ function update
         return 1
     end
 
+    # Check for a directory argument
+    set repo_dir $argv[1]
+    if test -z "$repo_dir"
+        echo "No directory specified. Use: sudo update <directory>"
+        return 1
+    end
+
+    # Replace `~` with `$HOME` to point to the correct user's home directory
+    if test "$repo_dir" = "~"
+        set repo_dir $HOME
+    end
+
+    # Expand the path if it's a relative path
+    set repo_dir (realpath $repo_dir)
+
     # System Package Updates
     command dnf upgrade --refresh --best --allowerasing
     command dnf update -v
@@ -12,24 +27,36 @@ function update
 
     # Docker Updates
     if type -q docker-compose
+        echo ==============================
         echo "Checking for Docker Compose files..."
+        echo ==============================
         for compose_file in (find ~ -name "docker-compose.yml")
             set compose_dir (dirname $compose_file)
+            echo ==============================
             echo "Pulling Docker Compose images in $compose_dir..."
+            echo ==============================
+
             cd $compose_dir
             docker-compose pull
         end
     end
 
     if type -q docker
+        echo ==============================
         echo "Pulling individual Docker images..."
+        echo ==============================
+
         for image in (docker images --format "{{.Repository}}:{{.Tag}}")
+            echo ==============================
             echo "Pulling $image..."
+            echo ==============================
             docker pull $image
         end
-
+        echo ==============================
         echo "Removing unused Docker images and volumes..."
-        docker image prune -af  
+        echo ==============================
+
+        docker image prune -af
         docker volume prune -f
     end
 
@@ -50,6 +77,32 @@ function update
     command rm -rf /tmp/*
     command journalctl --vacuum-time=2weeks
 
+    # Git Pull for Repositories
+    if test -d "$repo_dir"
+        for dir in (find $repo_dir -type d -name ".git" -exec dirname {} \;)
+            echo ==============================
+            echo "Updating Git repository in $dir..."
+            echo ==============================
+
+            cd $dir
+            git pull
+
+            # Prune old references
+            echo ==============================
+            echo "Pruning old references in $dir..."
+            echo ==============================
+
+            git remote prune origin
+
+            # Optimize repository
+            echo ==============================
+            echo "Optimizing repository in $dir..."
+            echo ==============================
+
+            git gc
+        end
+    else
+        echo "The specified directory does not exist."
+        return 1
+    end
 end
-
-
--- fish/.config/fish/functions/update_all.fish
+++ fish/.config/fish/functions/update_all.fish
@@ -2,7 +2,7 @@ function update_all
     brew update -v
     brew upgrade -v
 
-    sudo update
+    sudo update ~/.repos
     brew cleanup -v
     echo ==============================
     df -h
