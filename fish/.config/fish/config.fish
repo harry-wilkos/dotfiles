8421ed852fb26b02847bfc7646eba0fca0ecaab3
harry-wilkos <hg456wilkos@gmail.com>
2024-10-24 23:38:56 +0100

    more folders for auto env

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -37,13 +37,18 @@ function __auto_source_venv --on-variable PWD --description "Activate/Deactivate
     if git rev-parse --show-toplevel &>/dev/null
         set gitdir (realpath (git rev-parse --show-toplevel))
         set cwd (pwd -P)
+
+        # Array of possible virtual environment folder names
+        set venv_folders env venv .env .venv
+
         while string match "$gitdir*" "$cwd" &>/dev/null
-            if test -e "$cwd/env/bin/activate.fish"
-                source "$cwd/env/bin/activate.fish" &>/dev/null
-                return
-            else
-                set cwd (path dirname "$cwd")
+            for venv_folder in $venv_folders
+                if test -e "$cwd/$venv_folder/bin/activate.fish"
+                    source "$cwd/$venv_folder/bin/activate.fish" &>/dev/null
+                    return
+                end
             end
+            set cwd (path dirname "$cwd")
         end
     end
     if test -n "$VIRTUAL_ENV"
